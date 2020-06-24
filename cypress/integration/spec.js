7d5c85ac9e98bf0f97e504e3a5f775e7b1bce681
Kamran Ayub <kamran.ayub@gmail.com>
2020-06-24 00:19:22 -0500

    reproduce issue

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("works", () => {
+    cy.visit("/app/account");
+    // wait for StackBlitz to boot up server
+    cy.get("ion-button", { timeout: 60 * 1000 }).click();
+    cy.get("ion-popover").should("be.visible");
+  });
+});
